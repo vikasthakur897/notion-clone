b30ac9eb15d89766ce4104b7b865c518a54e8bab
VikasThakur <vikasthakur1723@gmail.com>
2025-05-30 23:26:56 +0530

    Add restore button and his functinality

--- app/(main)/_component/trash-box.tsx
+++ app/(main)/_component/trash-box.tsx
@@ -3,99 +3,111 @@ import { useMutation, useQuery } from "convex/react";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
 import { useState } from "react";
-import  { Id } from "@/convex/_generated/dataModel";
+import { Id } from "@/convex/_generated/dataModel";
 import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
-import { Search } from "lucide-react";
+import { Search, Trash, Undo } from "lucide-react";
 import { Input } from "@/components/ui/input";
 
 const TrashBox = () => {
+  const router = useRouter();
 
-    const router = useRouter();
-    
-    const param = useParams();
-    const document = useQuery(api.documnets.getTrash)
-    const restore = useMutation(api.documnets.restore);
+  const param = useParams();
+  const document = useQuery(api.documnets.getTrash);
+  const restore = useMutation(api.documnets.restore);
 
-    const remove = useMutation(api.documnets.remove);
+  const remove = useMutation(api.documnets.remove);
 
-    const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
 
-    const filteredDocuments = document?.filter((document) => {
-        return document.title.toLowerCase().includes(search.toLowerCase());
-    })
+  const filteredDocuments = document?.filter((document) => {
+    return document.title.toLowerCase().includes(search.toLowerCase());
+  });
 
-    const onClick = (documentId: string) =>{
-        router.push(`/documents/${documentId}`);
+  const onClick = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
+
+  const onRestore = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<"documents">
+  ) => {
+    event.stopPropagation();
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored successfully",
+      error: "Failed to restore note.",
+    });
+  };
+
+  const onRemove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<"documents">
+  ) => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Deleting note...",
+      success: "Note delete successfully",
+      error: "Failed to delete note.",
+    });
+
+    if (param.documentId === documentId) {
+      router.push("/documents");
     }
+  };
 
-    const onRestore = (
-        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        documentId: Id<"documents">
-    ) => {
-        event.stopPropagation();
-       const promise = restore({ id: documentId });
+  if (document === undefined) {
+    return (
+      <div className="h-full flex item-center p-4">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
 
-       toast.promise(promise, {
-            loading: "Restoring note...",   
-            success: "Note restored successfully",
-            error: "Failed to restore note.",})
-    };
+  return (
+    <div className="text-sm">
+      <div className="flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg=secondary"
+          placeholder="Filter by page title..."
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+          No documents found
+        </p>
+        {filteredDocuments?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+          >
+            <span className="trancate pl-2">{document.title}</span>
 
-     const onRemove = (
-        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        documentId: Id<"documents">
-    ) => {
-       const promise = remove({ id: documentId });
-
-       toast.promise(promise, {
-            loading: "Deleting note...",   
-            success: "Note delete successfully",
-            error: "Failed to delete note.",});
-
-            if(param.documentId === documentId){
-                router.push("/documents");
-            }
-    };
-
-    if(document === undefined){
-
-        return (
-            <div className="h-full flex item-center p-4">
-                <Spinner size="lg" />
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, document._id)}
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div role="button" className="rounded-sm p-2 hover:bg-neutral-200">
+                <Trash className="h-4 w-4 text-muted-foreground" />
+              </div>
             </div>
-        )
-    }
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="text-sm">
-            <div className="flex items-center gap-x-1 p-2">
-                <Search className="h-4 w-4" />
-                <Input
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  className="h-7 px-2 focus-visible:ring-transparent bg=secondary"
-                  placeholder="Filter by page title..."
-                />
-            </div>
-            <div className="mt-2 px-1 pb-1">
-                <p className="hidden last:block text-xs text-center text-muted-foreground pb-2" >
-                    No documents found
-                </p>
-                {filteredDocuments?.map((document) =>(
-                    <div
-                    key={document._id}
-                    role="button"
-                    onClick={() => onClick(document._id)}
-                    className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between">
-                        <span>
-                            {document.title}
-                        </span>
-                    </div>
-                ))}
-            </div>
-        </div>
-     );
-}
- 
 export default TrashBox;
