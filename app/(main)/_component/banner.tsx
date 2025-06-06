c4399d1de443ec92a6fdfabe71cac9e9aa48c63a
VikasThakur <vikasthakur1723@gmail.com>
2025-06-06 22:39:12 +0530

    Add Banner template in the application

---
+++ app/(main)/_component/banner.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+
+interface BannerProps {
+    documentId : Id<"documents">
+}
+
+const Banner = ({
+    documentId
+}: BannerProps) => {
+    return ( 
+        <div>
+            Banner
+        </div>
+     );
+}
+ 
+export default Banner;
--- app/(main)/_component/navbar.tsx
+++ app/(main)/_component/navbar.tsx
@@ -6,47 +6,50 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import Title from "./title";
+import Banner from "./banner";
 
 interface NavbarProps {
-    isCollapsed: boolean;
-    onResetWidth: () => void;
+  isCollapsed: boolean;
+  onResetWidth: () => void;
 }
 
-const Navbar = ({
-    isCollapsed,
-    onResetWidth
-}: NavbarProps) => {
-    const params = useParams();
+const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+  const params = useParams();
 
-    const document = useQuery(api.documnets.getById, {
-        documentId: params.documentId as Id<"documents">
-    });
+  const document = useQuery(api.documnets.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
 
-    if(document === undefined){
-        return (
-           <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center">
-            <Title.Skeleton />
-           </nav> 
-        )
-    }
-    if(document === null){
-        return null;
-    }
-    return ( 
-        <>
-        <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
-            {isCollapsed && (
-                <MenuIcon
-                role="button"
-                onClick={onResetWidth}
-                className="h-6 w-6 text-muted-foreground" />
-            )}
-            <div className="flex items-center justify-between w-full">
-               <Title initialDate={document} />
-            </div>
-        </nav>
-        </>
-     );
-}
- 
+  if (document === undefined) {
+    return (
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center">
+        <Title.Skeleton />
+      </nav>
+    );
+  }
+  if (document === null) {
+    return null;
+  }
+  return (
+    <>
+      <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialDate={document} />
+        </div>
+      </nav>
+
+      {document.isArchived && (
+        <Banner documentId={ document._id } />
+      )}
+    </>
+  );
+};
+
 export default Navbar;
