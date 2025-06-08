ba0f4cb94b44f07dd7ba49a96be44fa4c87863ee
VikasThakur <vikasthakur1723@gmail.com>
2025-06-08 17:58:34 +0530

    Add menu component

---
+++ app/(main)/_component/menu.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+
+interface MenuProps {
+    documentId : Id<"documents">;
+}
+
+export const Menu = ({
+    documentId
+}: MenuProps) => {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
