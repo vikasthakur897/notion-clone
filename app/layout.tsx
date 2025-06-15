88a5324432b1c44477a85bf852d19e97dfa8a619
VikasThakur <vikasthakur1723@gmail.com>
2025-06-15 13:48:16 +0530

    fix the clerk auth and router navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,20 @@ import { Toaster } from "sonner";
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Geist, Geist_Mono, } from 'next/font/google'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,23 +45,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ConvexClientProvider>
-        <EdgeStoreProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="jotion-theme-2"
-          >
-            <Toaster position="bottom-center" />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
-          </EdgeStoreProvider>
-        </ConvexClientProvider>
-      </body>
-    </html>
+  <body className={` ${inter.className} ${geistSans.className} ${geistMono.className} antialiased`}>
+    <ConvexClientProvider>
+      <EdgeStoreProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme-2"
+        >
+          <Toaster position="bottom-center" />
+          <ModalProvider />
+          {children}
+        </ThemeProvider>
+      </EdgeStoreProvider>
+    </ConvexClientProvider>
+  </body>
+</html>
+
   )
 }
--- components/search-command.tsx
+++ components/search-command.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { File } from "lucide-react";
 import { useQuery } from "convex/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
 
 import {
